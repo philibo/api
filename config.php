2778f71b306a9c80f34cc970b4a8bf71f767ad6e
phili <cleusewitz9@gmail.com>
2016-08-18 17:01:44 +0800

    Signed-off-by: phili <cleusewitz9@gmail.com>

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+//配置文件
+header('Content-Type: text/html; charset=UTF-8');
+$github_k='70d4f381e4534a7a6e32'; //GitHub应用Client ID
+$github_s='df4f3c66cb035443d27713a34005188a850b0554'; //GitHub应用Client Secret
+$callback_url='https://github.com/philibo/api/callback.php'; //授权回调网址
